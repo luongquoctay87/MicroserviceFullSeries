2cfe1f0eb1f501af68b4b9b41b2450bc26a8347d
Tây Java <quoctay87@gmail.com>
2024-09-20 11:10:31 +0700

    api-gateway

--- codebase/api-gateway/src/main/java/vn/tayjava/ApiGatewayApplication.java
+++ codebase/api-gateway/src/main/java/vn/tayjava/ApiGatewayApplication.java
@@ -2,11 +2,19 @@ package vn.tayjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to Tây Java";
     }
 }
